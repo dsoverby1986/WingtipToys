13019c970840678fa6d98778e6c628f5b22e9922
Shane Overby <dsoverby1986@gmail.com>
2017-01-07 01:05:25 -0500

    Initial - My first Web Forms project
    Initial Commit - My first Web Forms project

---
+++ WingtipToys/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+
+        public Nullable<double> Price { get; set; }
+
+        public Nullable<int> CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
